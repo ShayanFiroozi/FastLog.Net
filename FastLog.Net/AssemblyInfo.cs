ed2d45e73f8f6838fbec1e6f528605e9ee1f82a6
Shayan Firoozi <shayan.firoozi@gmail.com>
2023-12-24 22:37:03 +0330

    Integrate AssemblyInfos Remove Control-Panel useless project.

--- FastLog.Net/AssemblyInfo.cs
+++ FastLog.Net/AssemblyInfo.cs
@@ -21,34 +21,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("High performance logger for .NET")]
-[assembly: AssemblyDescription("High performance logger for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TrendSoft® , Bandar Abbas , Iran")]
-[assembly: AssemblyProduct("TrendSoft® FastLog.Net")]
-[assembly: AssemblyCopyright("Copyright © 2023 TrendSoft® (MIT License)")]
-[assembly: AssemblyTrademark("Shayan Firoozi")]
-[assembly: AssemblyCulture("")]
-
-
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("EB259611-F3F2-4445-AA65-2E599FABB172")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-//[assembly: AssemblyFileVersion("3.7.99.111")]
+[assembly: AssemblyTitle("TrendSoft® High performance logger for .NET")]
+[assembly: AssemblyDescription("ثبت کننده وقایع برای دات نت")]
+[assembly: AssemblyProduct("ثبت کننده وقایع برای دات نت")]
